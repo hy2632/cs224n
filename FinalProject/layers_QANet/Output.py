27f7c293e8eee765887a01d377827f1e68f017f3
Ubuntu <hy2632@hy2632.fezvcokgijweleureqbeq5gonb.bx.internal.cloudapp.net>
2020-08-14 07:44:37 +0000

    08/14 可以训练

--- FinalProject/layers_QANet/Output.py
+++ FinalProject/layers_QANet/Output.py
@@ -5,21 +5,17 @@ from util import masked_softmax
 
 
 class Output(nn.Module):
-    def __init__(self, d_model, drop_prob=0.1):
+    def __init__(self, d_model):
         super().__init__()
         self.w1 = nn.Linear(2 * d_model, 1)
         self.w2 = nn.Linear(2 * d_model, 1)
-        self.dropout = nn.Dropout(drop_prob)
 
     def forward(self, M0, M1, M2, mask):
         # 08/10: mod: (batch_size, seq_len, h)
         # mask: (batch_size, seq_len, 1)
 
-        logits_1 = F.softmax(self.w1(torch.cat([M0, M1], dim=-1)), dim=-1)
-        logits_2 = F.softmax(self.w1(torch.cat([M0, M2], dim=-1)), dim=-1)
-
-        logits_1 = self.dropout(logits_1)
-        logits_2 = self.dropout(logits_2)
+        logits_1 = self.w1(torch.cat([M0, M1], dim=-1))
+        logits_2 = self.w1(torch.cat([M0, M2], dim=-1))
 
         # Shapes: (batch_size, seq_len)
         log_p1 = masked_softmax(logits_1.squeeze(), mask, log_softmax=True)
